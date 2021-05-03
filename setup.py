ffaf4f3013d7c29d7bbe914bdd12eeac4f8c571a
genbid007 <genbid007@gmail.com>
2021-05-03 11:57:25 +0530

    svm added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="src",
     version="0.0.1",
     description="its a wine Q package", 
-    author="c17hawke", 
+    author="genbid007", 
     packages=find_packages(),
     license="MIT"
 )
